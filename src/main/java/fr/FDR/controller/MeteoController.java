3e82ebfa6015c5ac54c424e69a9842a1d956026c
flodro34 <f.derosso@gmail.com>
2024-07-31 16:15:03 +0200

    feat(btn_meteo)call fct on click

---
+++ src/main/java/fr/FDR/controller/MeteoController.java
@@ -0,0 +1,11 @@
+package fr.FDR.controller;
+
+import javafx.event.ActionEvent;
+
+public class MeteoController {
+    public void displaytext(ActionEvent actionEvent) {
+        System.out.println("bonjour !");
+    }
+
+    //sera executer par fxml
+}
---
+++ src/main/java/fr/FDR/pojo/Meteo.java
@@ -0,0 +1,4 @@
+package fr.FDR.pojo;
+
+public class Meteo {
+}
